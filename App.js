15022efe9c9ef92e60fc027e5a634117ae06099b
Betsy Groton <grotoned@gmail.com>
2020-11-24 11:41:18 -0500

    chore: cleaned up unnecessary code and comments

--- App.js
+++ App.js
@@ -10,22 +10,9 @@
  */
 
 import React, { Component } from 'react';
-import {
-	AppRegistry,
-	View,
-	StyleSheet,
-	PixelRatio,
-	TouchableHighlight,
-	TouchableOpacity,
-	Text,
-	Dimensions,
-} from 'react-native';
+import { View, TouchableOpacity, Text, Dimensions } from 'react-native';
 
-import {
-	ViroVRSceneNavigator,
-	ViroARSceneNavigator,
-	ViroARScene,
-} from 'react-viro';
+import { ViroARSceneNavigator } from 'react-viro';
 
 /*
  TODO: Insert your API key below
@@ -36,15 +23,7 @@ var sharedProps = {
 
 // Sets the default scene you want for AR and VR
 var InitialARScene = require('./js/HelloWorldSceneAR');
-var InitialVRScene = require('./js/HelloWorldScene');
 
-var UNSET = 'UNSET';
-var VR_NAVIGATOR_TYPE = 'VR';
-var AR_NAVIGATOR_TYPE = 'AR';
-
-// This determines which type of experience to launch in, or UNSET, if the user should
-// be presented with a choice of AR or VR. By default, we offer the user a choice.
-var defaultNavigatorType = UNSET;
 
 //added by betsy
 const { width, height } = Dimensions.get('window');
@@ -62,13 +41,7 @@ export default class ViroSample extends Component {
 			flashMessage: false,
 			videoUrl: null,
 		};
-		this._getExperienceSelector = this._getExperienceSelector.bind(this);
-		this._getARNavigator = this._getARNavigator.bind(this);
-		this._getVRNavigator = this._getVRNavigator.bind(this);
-		this._getExperienceButtonOnPress = this._getExperienceButtonOnPress.bind(
-			this
-		);
-		this._exitViro = this._exitViro.bind(this);
+
 
 		//added by betsy
 		this._setARNavigatorRef = this._setARNavigatorRef.bind(this);
@@ -88,7 +61,7 @@ export default class ViroSample extends Component {
 					videoUrl: 'file://' + retDict.url,
 				});
 
-				// this.props.addPhoto(this.state.videoUrl);
+
 			});
 	}
 
@@ -110,27 +83,17 @@ export default class ViroSample extends Component {
 		});
 	}
 
-	// Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-	// if you are building a specific type of experience.
-	render() {
-		if (this.state.navigatorType == UNSET) {
-			return this._getExperienceSelector();
-		} else if (this.state.navigatorType == VR_NAVIGATOR_TYPE) {
-			return this._getVRNavigator();
-		} else if (this.state.navigatorType == AR_NAVIGATOR_TYPE) {
-			return this._getARNavigator();
-		}
-	}
 
-	// Presents the user with a choice of an AR or VR experience
-	_getExperienceSelector() {
+	render() {
+
 		return (
 			<View
 				style={{
 					width: width,
 					height: height,
 				}}
-			>
+      >
+        {/* for the AR scene to render */}
 				<View
 					style={{
 						position: 'absolute',
@@ -158,9 +121,7 @@ export default class ViroSample extends Component {
 						alignItems: 'center',
 						width: 58,
 						height: 58,
-						// top: 0,
 						bottom: 25,
-
 						transform: [{ translate: [80, 0, 0] }],
 					}}
 				>
@@ -196,99 +157,6 @@ export default class ViroSample extends Component {
 			</View>
 		);
 	}
-
-	// Returns the ViroARSceneNavigator which will start the AR experience
-	_getARNavigator() {
-		return (
-			<ViroARSceneNavigator
-				{...this.state.sharedProps}
-				initialScene={{ scene: InitialARScene }}
-			/>
-		);
-	}
-
-	// Returns the ViroSceneNavigator which will start the VR experience
-	_getVRNavigator() {
-		return (
-			<ViroVRSceneNavigator
-				{...this.state.sharedProps}
-				initialScene={{ scene: InitialVRScene }}
-				onExitViro={this._exitViro}
-			/>
-		);
-	}
-
-	// This function returns an anonymous/lambda function to be used
-	// by the experience selector buttons
-	_getExperienceButtonOnPress(navigatorType) {
-		return () => {
-			this.setState({
-				navigatorType: navigatorType,
-			});
-		};
-	}
-
-	// This function "exits" Viro by setting the navigatorType to UNSET.
-	_exitViro() {
-		this.setState({
-			navigatorType: UNSET,
-		});
-	}
 }
-
-var localStyles = StyleSheet.create({
-	viroContainer: {
-		flex: 1,
-		backgroundColor: 'black',
-	},
-	outer: {
-		flex: 1,
-		flexDirection: 'row',
-		alignItems: 'center',
-		backgroundColor: 'black',
-	},
-	inner: {
-		flex: 1,
-		flexDirection: 'column',
-		alignItems: 'center',
-		backgroundColor: 'black',
-	},
-	titleText: {
-		paddingTop: 30,
-		paddingBottom: 20,
-		color: '#fff',
-		textAlign: 'center',
-		fontSize: 25,
-	},
-	buttonText: {
-		color: '#fff',
-		textAlign: 'center',
-		fontSize: 20,
-	},
-	buttons: {
-		height: 80,
-		width: 150,
-		paddingTop: 20,
-		paddingBottom: 20,
-		marginTop: 10,
-		marginBottom: 10,
-		backgroundColor: '#68a0cf',
-		borderRadius: 10,
-		borderWidth: 1,
-		borderColor: '#fff',
-	},
-	exitButton: {
-		height: 50,
-		width: 100,
-		paddingTop: 10,
-		paddingBottom: 10,
-		marginTop: 10,
-		marginBottom: 10,
-		backgroundColor: '#68a0cf',
-		borderRadius: 10,
-		borderWidth: 1,
-		borderColor: '#fff',
-	},
-});
 
 module.exports = ViroSample;
